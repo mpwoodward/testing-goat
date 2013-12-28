645976c73f4110bdf2d8b16f961a897570cd2cd2
Matt Woodward <matt@mattwoodward.com>
2013-12-28 08:10:23 -0800

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -82,3 +82,17 @@ class NewVisitorTest(LiveServerTestCase):
 
         # satisfied, she goes back to sleep
         self.browser.quit()
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # she notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_tag_name('input')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=3)
+
+        # she starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_tag_name('input')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=3)
